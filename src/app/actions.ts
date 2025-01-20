3f545f6ffa54996cfea75285b0bda97f7159719b
Damiano Jack Coccia <coccia.damianoj@gmail.com>
2025-01-20 19:54:48 +0100

    redis updated action implemented

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,23 @@
 'use server'
 
 import { Redis } from "@upstash/redis";
+import { Message } from "ai";
 
 const redis = Redis.fromEnv();
 
 export async function fetchChats(user_id: string) {
     const chats = await redis.json.get('chats_' + user_id);
     return chats;
+};
+
+export async function postMessages(user_id: string, chat_id: number, messages: Message[]) {
+    function transformMessagesToRecords(messages: Message[]): Record<string, unknown>[] {
+        return messages.map((message) => ({
+          id: message.id,
+          content: message.content,
+          role: message.role,
+        }));
+    }
+    const records = transformMessagesToRecords(messages);
+    redis.json.set('chats_' + user_id, `$.${chat_id}.messages`, records);
 };
